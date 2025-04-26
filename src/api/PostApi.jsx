d61dfd16e1612250587d5f431364200db727274b
tiwariharshitraj14 <tiwariharshitraj28@gmail.com>
2025-04-26 18:05:56 +0530

    feat: add delete functionality - added deletePost API method - updated Posts component to handle post deletion

--- src/api/PostApi.jsx
+++ src/api/PostApi.jsx
@@ -8,3 +8,8 @@ const api = axios.create({
 export const getPost = () => {
   return api.get("/posts");
 };
+
+// delete method
+export const deletePost = (id) => {
+  return api.delete(`/posts/${id}`);
+};
--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -1,13 +1,30 @@
 import { useEffect } from "react";
-import { getPost } from "../api/PostApi";
+import { deletePost, getPost } from "../api/PostApi";
 import { useState } from "react";
 
 const Posts = () => {
   const [postData, setPostData] = useState([]);
+
   const getPostData = async () => {
     try {
       const res = await getPost();
       setPostData(res.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const handleDeletePost = async (id) => {
+    try {
+      const res = await deletePost(id);
+      if (res.status === 200) {
+        const newUpdatePost = postData.filter((delPost) => {
+          return delPost.id != id;
+        });
+        setPostData(newUpdatePost);
+      } else {
+        console.log("Failed to delete the post:", res.status);
+      }
     } catch (error) {
       console.log(error);
     }
@@ -25,7 +42,12 @@ const Posts = () => {
             <p>Title: {data.title}</p>
             <p>Body: {data.body}</p>
             <button>Edit</button>
-            <button className="btn-delete">Delete</button>
+            <button
+              className="btn-delete"
+              onClick={() => handleDeletePost(data.id)}
+            >
+              Delete
+            </button>
           </li>
         ))}
       </ol>
