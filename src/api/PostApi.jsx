10948a9e6a62003733ff34484e76a0a6f8554d3c
tiwariharshitraj14 <tiwariharshitraj28@gmail.com>
2025-04-26 13:32:48 +0530

    feat: integrate API call to fetch posts using Axios chore: remove unwanted code and update HTML page title

---
+++ src/api/PostApi.jsx
@@ -0,0 +1,10 @@
+import axios from "axios";
+
+const api = axios.create({
+  baseURL: "https://jsonplaceholder.typicode.com",
+});
+
+// get method
+export const getPost = () => {
+  return api.get("/posts");
+};
